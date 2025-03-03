85b31ae1fac94c7d9cde3d56313ef2a58d43c154
Naga Phanindra Dasari <104689319+Phani-LP@users.noreply.github.com>
2025-03-03 12:59:03 +0530

    Delete ConverseAI's file

--- ConverseAI/variables.py
+++
@@ -1,9 +0,0 @@
-name = 'MYSQLDB'
-user = 'root'
-password = 'Maheswar^43'
-host = 'localhost'
-port = 3306
-
-
-API_KEY = "AIzaSyCEMT7kEweo6zz7EAG3kd8Oc1r1ChiCjdY"
-API_KEY2_Dont_work = "AIzaSyDM7a57V5Zjyk7fJG0BMxeyg38CWtBin-k"
